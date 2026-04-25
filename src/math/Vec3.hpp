1fb15b822c17b1ecda7d6bc5e0686d5374598c2e
Niquimi <genasdenic@gmail.com>
2026-04-25 18:51:42 +0200

    fix: delete wrong operations

--- src/math/Vec3.hpp
+++ src/math/Vec3.hpp
@@ -8,23 +8,6 @@ struct Vec3
 
     Vec3(float x, float y, float z) : x(x), y(y), z(z) {};
 
-    Vec3 operator+(const Vec3& other) const {
-        return Vec3(x + other.x, y + other.y, z + other.z);
-    };
-
-    Vec3 operator-(const Vec3& other) const {
-        return Vec3(x - other.x, y - other.y, z - other.z);
-    };
-
-    Vec3 operator*(const Vec3& other) const {
-        return Vec3(x * other.x, y * other.y, z * other.z);
-    };
-
-    Vec3 operator/(const Vec3& other) const {
-        if (other.x == 0 || other.y == 0 || other.z == 0) {
-            return Vec3(x, y, z);
-        }
-        return Vec3(x / other.x, y / other.y, z / other.z);
-    }
+    
 
 };
